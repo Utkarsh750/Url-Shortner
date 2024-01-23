0bb326bde0a321b9c0bb216582a3aa480a7a041c
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-01-24 03:27:11 +0530

    added shortId and visitHistory

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const { connectToMongoDB } = require("./connection");
 
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+
 const app = express();
 
 const PORT = 8001;
@@ -11,7 +13,26 @@ connectToMongoDB(
   "mongodb+srv://utkarsh750:1234@cluster0.wkygvry.mongodb.net/?retryWrites=true&w=majority/practise"
 ).then(() => console.log("MongoDb connected"));
 
+app.use(express.json());
+
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
 });
