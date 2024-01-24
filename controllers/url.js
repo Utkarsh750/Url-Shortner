aa7a09257ba870743db7adce73bb4813c5737c9b
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-01-25 02:26:50 +0530

    added analytics routes

--- controllers/url.js
+++ controllers/url.js
@@ -16,6 +16,15 @@ async function handleGenerateNewShortUrl(req, res) {
   return res.json({ id: shortID });
 }
 
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
 module.exports = {
   handleGenerateNewShortUrl,
+  handleGetAnalytics,
 };
--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { handleGenerateNewShortUrl } = require("../controllers/url");
+const {
+  handleGenerateNewShortUrl,
+  handleGetAnalytics,
+} = require("../controllers/url");
 const router = express.Router();
 
 router.post("/", handleGenerateNewShortUrl);
 
+router.get("/analytics/:shortId", handleGetAnalytics);
 module.exports = router;
