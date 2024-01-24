a4ba2dc160c2788a2434705a295dcd81107e0aa0
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-01-25 02:16:13 +0530

    added post and get request

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+mongoose.set("strictQuery", true);
 async function connectToMongoDB(url) {
   return mongoose.connect(url);
 }
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = 8001;
 
 connectToMongoDB(
-  "mongodb+srv://utkarsh750:1234@cluster0.wkygvry.mongodb.net/?retryWrites=true&w=majority/practise"
+  "mongodb+srv://utkarsh750:1234@cluster0.wkygvry.mongodb.net/?retryWrites=true"
 ).then(() => console.log("MongoDb connected"));
 
 app.use(express.json());
