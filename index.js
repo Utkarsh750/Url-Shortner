dda13a417c110c7d5166ee946b65a91945b6fdea
utkarsh750 <sharmautkarsh750.snp@gmail.com>
2024-01-26 16:31:57 +0530

    added ejs

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require("express");
-
+const path = require("path");
 const { connectToMongoDB } = require("./connection");
 
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
 
 const app = express();
-
 const PORT = 8001;
 
 connectToMongoDB(
   "mongodb+srv://utkarsh750:1234@cluster0.wkygvry.mongodb.net/?retryWrites=true"
 ).then(() => console.log("MongoDb connected"));
 
+app.set(" view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home");
+});
 
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
